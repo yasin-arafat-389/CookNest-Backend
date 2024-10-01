0b5419fbc06484e69a4d10e3032a3d034edf8931
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-10-01 22:36:59 +0600

    Added some necessary services in user module

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,7 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { JwtPayload } from 'jsonwebtoken';
 import { TUser } from './user.interface';
 import UserModel from './user.model';
 import RecipeModel from '../recipe/recipe.model';
+import {
+  initiatePayment,
+  verifyPayment,
+} from '../../utils/PaymentGateway/PaymentGateway';
 
 const createUser = async (payload: TUser) => {
   const isUserAlreadyExists = await UserModel.findOne({ email: payload.email });
@@ -19,6 +24,12 @@ const getSingleUser = async (id: string) => {
   const userPostedRecipeData = await RecipeModel.find({ user: id });
 
   return { userData, userPostedRecipeData };
+};
+
+const getUserInfo = async (id: string) => {
+  const userData = await UserModel.findById(id);
+
+  return userData;
 };
 
 const updateProfile = async (userId: string, updateData: Partial<TUser>) => {
@@ -109,10 +120,273 @@ const removeFromFollowing = async (id: string, user: JwtPayload) => {
   return updatedUser;
 };
 
+const becomePremiumMember = async (payload: any) => {
+  const user = await UserModel.findById(payload.id);
+
+  if (user) {
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      payload.id,
+      { transactionId: payload.transactionId },
+      { new: true },
+    );
+
+    if (!updatedUser) {
+      throw new Error('Error updating user');
+    }
+
+    const initializePayment = await initiatePayment(payload);
+
+    return initializePayment;
+  } else {
+    throw new Error('User not found');
+  }
+};
+
+const paymentConfirmation = async (transactionId: string) => {
+  const verifyResponse = await verifyPayment(transactionId);
+
+  let result;
+  let message = '';
+
+  if (verifyResponse && verifyResponse.pay_status === 'Successful') {
+    result = await UserModel.findOneAndUpdate(
+      { transactionId },
+      {
+        premiumMembership: true,
+      },
+    );
+    message = 'Successfully Paid!';
+  } else {
+    message = 'Payment Failed!';
+  }
+
+  console.log(result);
+
+  let templateForSuccessfulPayment = `
+   <html>
+  <head>
+    <link
+      href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,400i,700,900&display=swap"
+      rel="stylesheet"
+    />
+  </head>
+  <style>
+    body {
+      text-align: center;
+      padding: 40px 0;
+      background: #ebf0f5;
+    }
+    h1 {
+      color: #88b04b;
+      font-family: 'Nunito Sans', 'Helvetica Neue', sans-serif;
+      font-weight: 900;
+      font-size: 40px;
+      margin-bottom: 10px;
+    }
+    p {
+      color: #404f5e;
+      font-family: 'Nunito Sans', 'Helvetica Neue', sans-serif;
+      font-size: 20px;
+      margin: 0;
+    }
+    i {
+      color: #9abc66;
+      font-size: 100px;
+      line-height: 200px;
+      margin-left: -15px;
+    }
+    .card {
+      background: white;
+      padding: 60px;
+      border-radius: 4px;
+      box-shadow: 0 2px 3px #c8d0d8;
+      display: inline-block;
+      margin: 0 auto;
+    }
+
+    button {
+      background-color: #fb8c00;
+      border: 1px solid rgb(209, 213, 219);
+      border-radius: 0.5rem;
+      color: white;
+      font-family:
+        ui-sans-serif,
+        system-ui,
+        -apple-system,
+        system-ui,
+        'Segoe UI',
+        Roboto,
+        'Helvetica Neue',
+        Arial,
+        'Noto Sans',
+        sans-serif,
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji';
+      font-size: 20px;
+      margin-top: 30px;
+      font-weight: 600;
+      line-height: 1.25rem;
+      padding: 0.75rem 1rem;
+      text-align: center;
+      -webkit-box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
+      box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
+      cursor: pointer;
+      -moz-user-select: none;
+      -ms-user-select: none;
+      user-select: none;
+      -webkit-user-select: none;
+      -ms-touch-action: manipulation;
+      touch-action: manipulation;
+    }
+  </style>
+  <body>
+    <div class="card">
+      <div
+        style="
+          border-radius: 200px;
+          height: 200px;
+          width: 200px;
+          background: #f8faf5;
+          margin: 0 auto;
+        "
+      >
+        <i class="checkmark">✓</i>
+      </div>
+      <h1>Payment Successful!</h1>
+
+      <button onclick="window.location.href='http://localhost:3000/dashboard';">
+        Go To Your Dashboard
+      </button>
+    </div>
+  </body>
+</html>
+
+  `;
+
+  const templateForFailedPayment = `
+   <html>
+  <head>
+    <link
+      href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,400i,700,900&display=swap"
+      rel="stylesheet"
+    />
+  </head>
+  <style>
+    body {
+      text-align: center;
+      padding: 40px 0;
+      background: #ebf0f5;
+    }
+    h1 {
+      color: red;
+      font-family: 'Nunito Sans', 'Helvetica Neue', sans-serif;
+      font-weight: 900;
+      font-size: 40px;
+      margin-bottom: 10px;
+    }
+    p {
+      color: #404f5e;
+      font-family: 'Nunito Sans', 'Helvetica Neue', sans-serif;
+      font-size: 20px;
+      margin: 0;
+    }
+    i {
+      color: red;
+      font-size: 100px;
+      line-height: 200px;
+      margin-left: -15px;
+    }
+    .card {
+      background: white;
+      padding: 60px;
+      border-radius: 4px;
+      box-shadow: 0 2px 3px #c8d0d8;
+      display: inline-block;
+      margin: 0 auto;
+    }
+
+    button {
+      background-color: #fb8c00;
+      border: 1px solid rgb(209, 213, 219);
+      border-radius: 0.5rem;
+      color: white;
+      font-family:
+        ui-sans-serif,
+        system-ui,
+        -apple-system,
+        system-ui,
+        'Segoe UI',
+        Roboto,
+        'Helvetica Neue',
+        Arial,
+        'Noto Sans',
+        sans-serif,
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji';
+      font-size: 20px;
+      margin-top: 30px;
+      font-weight: 600;
+      line-height: 1.25rem;
+      padding: 0.75rem 1rem;
+      text-align: center;
+      -webkit-box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
+      box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
+      cursor: pointer;
+      -moz-user-select: none;
+      -ms-user-select: none;
+      user-select: none;
+      -webkit-user-select: none;
+      -ms-touch-action: manipulation;
+      touch-action: manipulation;
+    }
+  </style>
+  <body>
+    <div class="card">
+      <div
+        style="
+          border-radius: 200px;
+          height: 200px;
+          width: 200px;
+          background: #f8faf5;
+          margin: 0 auto;
+        "
+      >
+        <i class="checkmark">❌</i>
+      </div>
+      <h1>Payment Failed!</h1>
+
+      <button onclick="window.location.href='http://localhost:3000';">
+        Go To Homepage
+      </button>
+    </div>
+  </body>
+</html>
+
+  `;
+
+  if (message === 'Successfully Paid!') {
+    return (templateForSuccessfulPayment = templateForSuccessfulPayment.replace(
+      '{{message}}',
+      message,
+    ));
+  }
+
+  if (message === 'Payment Failed!') {
+    return templateForFailedPayment.replace('{{message}}', message);
+  }
+};
+
 export const UserServices = {
   createUser,
   updateProfile,
   addToFollowing,
   removeFromFollowing,
   getSingleUser,
+  getUserInfo,
+  becomePremiumMember,
+  paymentConfirmation,
 };
